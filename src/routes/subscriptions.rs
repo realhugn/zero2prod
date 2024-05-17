973f82c88648bd5ae5595244d77d743eeb2a4e46
realHugn <nothung209@gmail.com>
2024-05-17 16:00:37 +0700

    add dockerfile (fail)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,17 @@ pub async fn subscribe(
     _form: web::Form<FormData>,
     _pool: web::Data<PgPool>
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %_form.email,
+        subscriber_name= %_form.name
+    );
+    let _request_span_guard = request_span.enter();
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
+
     match sqlx::query!(r#"
         insert into subscriptions (id, email, name, subscribed_at) 
             values ($1, $2, $3, $4);
@@ -26,9 +37,12 @@ pub async fn subscribe(
         .execute(_pool.as_ref())
         .await
         {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber details have been saved",request_id);
+            HttpResponse::Ok().body("Thanks you for your subscription")
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Frequest_id {} - ailed to execute query: {:?}",request_id, e);
             return HttpResponse::InternalServerError().finish()
         }
     }
